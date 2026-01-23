4543b31074bbc484b52c84d0f1fc8fb6bc3b1b33
arcshiftsolutions <arcshiftsolutions@gmail.com>
2026-01-22 17:39:33 -0800

    Dockerfile update

--- api/src/main/java/ca/bc/gov/educ/challenge.reports/api/util/SearchCriteriaBuilder.java
+++ api/src/main/java/ca/bc/gov/educ/challenge.reports/api/util/SearchCriteriaBuilder.java
@@ -78,7 +78,7 @@ public class SearchCriteriaBuilder {
         eqOrChallengeCrit.put("condition", "AND");
 
         Map<String, Object> finalPercentCrit = new HashMap<>();
-        finalPercentCrit.put("key", "finalPercentage");
+        finalPercentCrit.put("key", "finalPercent");
         finalPercentCrit.put("operation", "gt");
         finalPercentCrit.put("value", "49");
         finalPercentCrit.put("valueType", "INTEGER");
